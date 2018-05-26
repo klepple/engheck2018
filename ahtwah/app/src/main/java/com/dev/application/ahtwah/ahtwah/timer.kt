304f5e6bc4a807ef662725f77bd2b32cdac7ae39
John Huang <johnjunhh@gmail.com>
2018-05-26 00:05:13 -0400

    added almost everything so far including the main activity UI

---
+++ ahtwah/app/src/main/java/com/dev/application/ahtwah/ahtwah/timer.kt
@@ -0,0 +1,12 @@
+package com.dev.application.ahtwah.ahtwah
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class timer : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer)
+    }
+}
