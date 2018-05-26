a78d59a82e9ee3c5db09cd2188ddf178deeec318
Nuliflyer <tyler@hyperweb.ca>
2018-05-26 12:10:03 -0400

    pre-emptively implementing stuff

--- ahtwah/app/src/main/java/com/dev/application/ahtwah/ahtwah/create_room.kt
+++ ahtwah/app/src/main/java/com/dev/application/ahtwah/ahtwah/create_room.kt
@@ -3,7 +3,13 @@ package com.dev.application.ahtwah.ahtwah
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class create_room : AppCompatActivity() {
 
@@ -12,6 +18,23 @@ class create_room : AppCompatActivity() {
         setContentView(R.layout.activity_create_room)
         val createButton = findViewById<Button>(R.id.joinbtn)
         createButton.setOnClickListener{
+            val queue = Volley.newRequestQueue(this)
+            val url = "KAHLIA PLZ"
+            val response = Response.Listener<String>{
+                response ->
+                val code = "KAHLIA PLZ"
+                val intent = Intent(this, room_settings::class.java)
+                intent.putExtra("Roomcode", code)
+                startActivity(intent)
+            }
+            val error = Response.ErrorListener {
+                error ->
+                Log.d("Error", error.toString())
+                Toast.makeText(this, "Error, Something went wrong.", Toast.LENGTH_LONG)
+            }
+            val StringRequest = StringRequest(Request.Method.GET, url, response, error)
+            queue.add(StringRequest)
+
             val roomSettingsIntent = Intent(this,room_settings::class.java)
             startActivity(roomSettingsIntent)
         }
--- ahtwah/app/src/main/java/com/dev/application/ahtwah/ahtwah/join_room.kt
+++ ahtwah/app/src/main/java/com/dev/application/ahtwah/ahtwah/join_room.kt
@@ -1,12 +1,24 @@
 package com.dev.application.ahtwah.ahtwah
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class join_room : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join_room)
+
+        val userText = findViewById<Button>(R.id.usernameText)
+        val roomText = findViewById<Button>(R.id.roomText)
+        val joinButton = findViewById<Button>(R.id.joinbtn)
+        joinButton.setOnClickListener{
+            val intent = Intent(this, room_settings::class.java)
+            intent.putExtra("Username", userText.text.toString())
+            intent.putExtra("RoomID", roomText.text.toString())
+            startActivity(intent)
+        }
     }
 }
