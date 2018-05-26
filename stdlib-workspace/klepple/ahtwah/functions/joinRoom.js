e98fb4e5183abeed4562bf1b2af537b7bf227825
Quin Millard <midnitefreddy@hotmail.com>
2018-05-26 15:17:30 -0400

    changed some things

--- stdlib-workspace/klepple/ahtwah/functions/joinRoom.js
+++ stdlib-workspace/klepple/ahtwah/functions/joinRoom.js
@@ -1,8 +1,6 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-let cache = null;
-
 /**
  * API so a new user can join the room
  * @param {string} username the username
@@ -21,39 +19,37 @@ module.exports = (username, timeLeft, roomId, context, callback) => {
     let uri = process.env['MONGO_URI'];
   
     try {
-        MongoClient.connect(uri, (error, db) => {
-          let db = client.db('ahtwahdb');
+        MongoClient.connect(uri, (error, client) => {
           if (error) {
             console.log(error['errors']);
             return callback(error);
           }
-          
-          createUser(db, user, callback);
+          //createUser(client.db('ahtwahdb'), user, callback);
         });
     } catch (error) {
       console.log(error);
       return callback(error);
     }
-    console.log(db);
+    
   };
 
   const createUser = (db, user, callback) => {
     //Update room object to reflect added user
-    db.collection('rooms').updateOne({ roomId: user.roomId }, { $inc: { numberOfUsers: 1} }, function(err, res) {
-        if (err) throw err;
-        console.log("Number of connected users updated.");
-    });
-    //Add user to the list of connected users
-    db.collection('rooms').updateOne({ roomId: user.roomId }, { $addToSet: { listOfConnectedUsers: user.username } }, function(err, res) {
-        if (err) throw err;
-        console.log("User added to the list of connected users");
-    });
-    db.collection('users').insertOne(user, (error, result) => {
-        if (error) {
-          console.log(error);
-          return callback(null, error);
-        }
-        let formattedResult = JSON.parse(JSON.stringify(result.insertedId));
-        return callback(null, user.roomId);
-      });
+    // db.collection('rooms').updateOne({ roomId: user.roomId }, { $inc: { numberOfUsers: 1} }, function(err, res) {
+    //     if (err) throw err;
+    //     console.log("Number of connected users updated.");
+    // });
+    // //Add user to the list of connected users
+    // db.collection('rooms').updateOne({ roomId: user.roomId }, { $addToSet: { listOfConnectedUsers: user.username } }, function(err, res) {
+    //     if (err) throw err;
+    //     console.log("User added to the list of connected users");
+    // });
+    // db.collection('users').insertOne(user, (error, result) => {
+    //     if (error) {
+    //       console.log(error);
+    //       return callback(null, error);
+    //     }
+    //     let formattedResult = JSON.parse(JSON.stringify(result.insertedId));
+    //     return callback(null, user.roomId);
+    //   });
   };
