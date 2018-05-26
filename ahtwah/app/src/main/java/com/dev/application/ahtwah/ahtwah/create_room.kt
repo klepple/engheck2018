cd3104c5e093e8c20abac15272312b9c432cfdf9
John Huang <johnjunhh@gmail.com>
2018-05-26 01:35:41 -0400

    adding create room page

--- ahtwah/app/src/main/java/com/dev/application/ahtwah/ahtwah/create_room.kt
+++ ahtwah/app/src/main/java/com/dev/application/ahtwah/ahtwah/create_room.kt
@@ -1,12 +1,19 @@
 package com.dev.application.ahtwah.ahtwah
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class create_room : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_room)
+        val createButton = findViewById<Button>(R.id.createbtn)
+        createButton.setOnClickListener{
+            val roomSettingsIntent = Intent(this,room_settings::class.java)
+            startActivity(roomSettingsIntent)
+        }
     }
 }
